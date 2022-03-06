3387f523926cd97ed018c4b19be6a4f340f1dec1
Jared <jmezzbmxer@live.ca>
2022-03-06 01:46:01 -0500

    added contact form submission methods

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,31 +1,80 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Fade from "react-reveal/Fade";
 import "./Contact.css"
 
-import { Button } from "../index"
+import { submitContact } from "../../api";
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: '', email: '', message: '', submitted: false};
+    }
+
+    handleNameChange = (event) => {
+        this.setState({name: event.target.value});
+    }
+
+    handleEmailChange = (event) => {
+        this.setState({email: event.target.value})
+    }
+    handleMessageChange = (event) => {
+        this.setState({message: event.target.value})
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event.target.name.value);
+        this.setState({ submitted: true })
+        let payload = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            message: event.target.message.value
+        }
+        submitContact(payload)
+            .then(r => console.log(r));
+    }
+
     render(){
-        return(
-            <section className="section contact" id="contact">
-                <div className="contact-container container grid">
-                    <div className="contact-data">
-                        <h2 className="section-title">Contact</h2>
-                        <Fade>
-                            <p className="contact-description">
-                                Please feel free to reach out, I'd love to get to know your project!
-                            </p>
-                            <form action="" className="contact-form">
-                                <input type="text" className="contact-input" name="name" id="name" placeholder="Name"/>
-                                <input type="email" className="contact-input" name="email" id="email" placeholder="Email"/>
-                                <textarea name="message" id="message" cols="30" rows="10" className="contact-input" placeholder="Let me know what you're working on!"></textarea>
-                                <Button buttonTitle="Send Message" buttonLink="#"/>
-                            </form>
-                        </Fade>
+        if (!this.state.submitted) {
+            return(
+                <section className="section contact" id="contact">
+                    <div className="contact-container container grid">
+                        <div className="contact-data">
+                            <h2 className="section-title">Contact</h2>
+                            <Fade>
+                                <p className="contact-description">
+                                    Please feel free to reach out, I'd love to get to know your project!
+                                </p>
+                                <form onSubmit={this.handleSubmit} className="contact-form">
+                                    <input type="text" className="contact-input" name="name" id="name" placeholder="Name" value={this.state.name} onChange={this.handleNameChange}/>
+                                    <input type="email" className="contact-input" name="email" id="email" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange}/>
+                                    <textarea name="message" id="message" cols="30" rows="10" className="contact-input" placeholder="Let me know what you're working on!" onChange={this.handleMessageChange} value={this.state.message}></textarea>
+                                    <input type="submit" value='Send Message' className='button' cur/>
+                                </form>
+                            </Fade>
+                        </div>
                     </div>
-                </div>
-            </section>
-        )
+                </section>
+            )
+        } else {
+            return (
+                <section className="section contact" id="contact">
+                    <div className="contact-container container grid">
+                        <div className="contact-data">
+                            <h2 className="section-title">Contact</h2>
+                            <Fade>
+                                <p className="contact-description">
+                                    Please feel free to reach out, I'd love to get to know your project!
+                                </p>
+                                <div className='contact-form'>
+                                    <h3>Thanks for filling this out, I'll be in touch shortly!</h3>
+                                </div>
+                            </Fade>
+                        </div>
+                    </div>
+                </section>
+            )
+        }
     }
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api'
+    baseURL: 'http://localhost:3000/portfolio'
 });
 
 export const submitContact = payload => api.post('/contact', payload);
