7cda8617166fc3c69fa76f6e332b7881aad5a586
Jared Mezzatesta <jared@jicdesign.com>
2022-08-13 19:43:04 -1000

    direct url attempt

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://localhost:8393/portfolio'
-    baseURL: 'http://srv-captain--api-server'
+    // baseURL: 'http://srv-captain--api-server' -- Mixed content error due to HTTPS -> HTTP
+    baseURL: 'srv-captain--api-server' // direct url, dont think this is going to work as it'll probably extend from current url.
 });
 
 export const testCall = payload => api.get('/');
